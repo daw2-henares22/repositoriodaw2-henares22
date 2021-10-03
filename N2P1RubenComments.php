e8869e426029ff1a5adc868a1a3291106b12905d
daw2-henares22 <91276223+daw2-henares22@users.noreply.github.com>
2021-10-03 16:38:50 +0200

    Add files via upload

---
+++ N2P1RubenComments.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<ol>	
+	<li> I have learned to use the GET element </li>
+	<li> I have learned to use the SESSION element </li>
+	<li> I have learned to use the POST element </li>
+	<li> I have learned to use the SESSION element </li>
+	<li> I have learned to use the COKKIE element </li>
+	<li> I have learned to use the LINK element </li>
+	<li> I have learned to use the NULL element </li>
+	<li> I have learned to use the DATE element </li>
+	<li> I have learned to use the URLENCODE element </li>
+	<li> I have learned to use the USERNAME element </li>
+</ol>
+<p>Document: 10. Teacher explanation: 10.</p>
+
+<p>Own note of the exercise: 10</p>
+
+<p>Some commands to put in the files, they were not right, if it gets wrong, I prefer that it not be put or make an example of the number of lines and the commands but not put it whole and wrong, if the teacher wants it to be Let's search the internet.</p>
+</body>
+</html>
---
+++ apartado2directivesvariablesoperadors.php
@@ -0,0 +1,32 @@
+<?php
+        //Enter your code here, enjoy!
+
+$array = array("1" => "PHP code tester Sandbox Online",  
+              "foo" => "bar", 5 , 5 => 89009, 
+              "case" => "Random Stuff: " . rand(100,999),
+              "PHP Version" => phpversion()
+              );
+              
+foreach( $array as $key => $value ){
+    echo $key."\t=>\t".$value."\n";
+    $_GET['favmovie'];
+    $_SESSION['authuser'];
+    $_POST["fname"];
+    $_COOKIE['cookiename'];
+    $myfavmovie = urlencode("Life of Brian");
+    "<a href='N2P11MovieSite.php?favmovie=$myfavmovie'>";
+    
+    $month = date('n');
+    if ($month == 1) { echo '31'; }
+    if ($month == 2) { echo '28 (unless it\'s a leap year)';}
+    if ($month == 3) { echo '31'; }
+    if ($month == 4) { echo '30'; }
+    if ($month == 5) { echo '31'; }
+    if ($month == 6) { echo '30'; }
+    if ($month == 7) { echo '31'; }
+    if ($month == 8) { echo '31'; }
+    if ($month == 9) { echo '30'; }
+    if ($month == 10) { echo '31'; }
+    if ($month == 11) { echo '30'; }
+    if ($month == 12) { echo '31'; }
+}
