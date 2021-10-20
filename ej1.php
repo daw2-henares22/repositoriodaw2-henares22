6f65eeed3f99f06a908cf31d0fd842d709b255b9
daw2-henares22 <91276223+daw2-henares22@users.noreply.github.com>
2021-10-20 18:02:49 +0200

    Delete ej1.php

--- ej1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', 'root') or
-    die ('Unable to connect. Check your connection parameters.');
-mysqli_select_db($db,'moviesite') or die(mysqli_error($db));
-
-$query = 'ALTER TABLE movie_leadactor
-ADD CONSTRAINT (people_id) FOREIGN KEY people(people_id)';
-Echo "Added data";
-?>
